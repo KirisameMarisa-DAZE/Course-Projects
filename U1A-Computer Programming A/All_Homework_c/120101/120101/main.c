0400661a679216505c3af3f51f4ff1116642ba95
Page <page@mail.ustc.edu.cn>
2025-02-24 20:55:06 +0800

    upload

---
+++ U1A-Computer Programming A/All_Homework_c/120101/120101/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  temp1
+//
+//  Created by 李佩哲 on 2021/9/22.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("题目：\n输出helloWorld\n以下是答案：\n");
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ U1A-Computer Programming A/All_Homework_c/120202/120102/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  temp2
+//
+//  Created by 李佩哲 on 2021/9/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("题目：\n修改上面的helloworld.c， 使它能够输出下面样式的字符串，\n*\n***\n*****\n*******\n*****\n***\n*\n以下是答案：\n");
+    printf("*\n***\n*****\n*******\n*****\n***\n*\n");
+    return 0;
+}
